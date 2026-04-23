0edbfd2a9fba6b9a69ac54a3077f65c52230c24c
Lucas Balieiro <lucas.macbali@gmail.com>
2026-04-23 19:37:13 -0300

    Add: LobbyMultiplayer Scene

--- Assets/Scripts/Multiplayer/SessionManager.cs
+++ Assets/Scripts/Multiplayer/SessionManager.cs
@@ -1,88 +1,114 @@
 using System;
-using Cysharp.Threading.Tasks;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Unity.Netcode;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Multiplayer;
-using UnityUtils;
 using UnityEngine;
+using UnityUtils;
 
-public class SessionManager : Singleton<SessionManager> {
-    ISession activeSession;
-
-    ISession ActiveSession {
+public class SessionManager : Singleton<SessionManager>
+{
+    private ISession activeSession;
+    public ISession ActiveSession
+    {
         get => activeSession;
-        set {
+        private set
+        {
             activeSession = value;
-            Debug.Log($"Active session: {activeSession}");
+            Debug.Log("Active Session: " + (activeSession != null ? activeSession.Id : "None"));
         }
     }
     
+    public string CurrentSessionCode => ActiveSession?.Code; 
+
     const string playerNamePropertyKey = "playerName";
     
-    async void Start() {
-        try {
+    private NetworkManager networkManager;
+    
+    async void Start()
+    {
+        try
+        {
             await UnityServices.InitializeAsync();
             await AuthenticationService.Instance.SignInAnonymouslyAsync();
-            Debug.Log($"Sign in anonymously succeeded! PlayerID: {AuthenticationService.Instance.PlayerId}");
-            
-            StartSessionAsHost();
+            Debug.Log("AuthenticationService Player ID: " + AuthenticationService.Instance.PlayerId);
         }
-        catch (Exception e) {
+        catch (Exception e)
+        {
             Debug.LogException(e);
         }
     }
 
-    async UniTask<Dictionary<string, PlayerProperty>> GetPlayerProperties() {
+    async UniTask<Dictionary<string, PlayerProperty>> GetPlayerPropertiesAsync()
+    {
         var playerName = await AuthenticationService.Instance.GetPlayerNameAsync();
         var playerNameProperty = new PlayerProperty(playerName, VisibilityPropertyOptions.Member);
-        return new Dictionary<string, PlayerProperty> { { playerNamePropertyKey, playerNameProperty } };
-    }
-
-    async void StartSessionAsHost() {
-        var playerProperties = await GetPlayerProperties(); 
         
-        var options = new SessionOptions {
-            MaxPlayers = 2,
-            IsLocked = false,
-            IsPrivate = false,
-            PlayerProperties = playerProperties 
-        }.WithDistributedAuthorityNetwork();
-        
-        ActiveSession = await MultiplayerService.Instance.CreateSessionAsync(options);
-        Debug.Log($"Session {ActiveSession.Id} created! Join code: {ActiveSession.Code}");
+        return new Dictionary<string, PlayerProperty>{{playerNamePropertyKey, playerNameProperty}};
     }
 
-    async UniTaskVoid JoinSessionById(string sessionId) {
-        ActiveSession = await MultiplayerService.Instance.JoinSessionByIdAsync(sessionId);
-        Debug.Log($"Session {ActiveSession.Id} joined!");
+    public async UniTask CreateSessionAsHost() 
+    {
+        try 
+        {
+            var playerProperties = await GetPlayerPropertiesAsync();
+
+            var options = new SessionOptions()
+            {
+                MaxPlayers = 2,
+                IsLocked = false,
+                IsPrivate = false,
+                PlayerProperties = playerProperties
+            }.WithDistributedAuthorityNetwork();
+            
+            ActiveSession = await MultiplayerService.Instance.CreateSessionAsync(options);
+            Debug.Log("Session ID: " + ActiveSession.Id);
+            Debug.Log("Session Code: " + ActiveSession.Code);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to create session: {e.Message}");
+        }
     }
 
-    async UniTaskVoid JoinSessionByCode(string sessionCode) {
-        ActiveSession = await MultiplayerService.Instance.JoinSessionByCodeAsync(sessionCode);
-        Debug.Log($"Session {ActiveSession.Id} joined!");
+    public async UniTask JoinSessionByCode(string code) 
+    {
+        try 
+        {
+            ActiveSession = await MultiplayerService.Instance.JoinSessionByCodeAsync(code);
+            Debug.Log("Successfully joined session!");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to join session: {e.Message}");
+        }
     }
 
-    async UniTaskVoid KickPlayer(string playerId) {
+    //TODO: Implementar no lobby
+    public async UniTask KickPlayer(string playerID)
+    {
         if (!ActiveSession.IsHost) return;
-        await ActiveSession.AsHost().RemovePlayerAsync(playerId);
+        
+        await ActiveSession.AsHost().RemovePlayerAsync(playerID);
     }
 
-    async UniTask<IList<ISessionInfo>> QuerySessions() {
-        var sessionQueryOptions = new QuerySessionsOptions();
-        QuerySessionsResults results = await MultiplayerService.Instance.QuerySessionsAsync(sessionQueryOptions);
-        return results.Sessions;
-    }
-
-    async UniTaskVoid LeaveSession() {
-        if (ActiveSession != null) {
-            try {
+    //TODO: Implementar no lobby
+    public async UniTask LeaveSession()
+    {
+        if (ActiveSession != null)
+        {
+            try
+            {
                 await ActiveSession.LeaveAsync();
             }
-            catch {
-                // Ignored as we are exiting the game
+            catch (Exception e)
+            {
+                Debug.LogException(e);
             }
-            finally {
+            finally
+            {
                 ActiveSession = null;
             }
         }
